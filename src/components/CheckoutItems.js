6f3bc4a748fa5d60384d571c39f5f7a5312669a4
Oguzhan <75084633+Oguzhansalkim@users.noreply.github.com>
2021-07-24 13:06:17 +0300

    Update CheckoutItems.js

--- src/components/CheckoutItems.js
+++ src/components/CheckoutItems.js
@@ -1,81 +1,75 @@
 import { StarIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 import Currency from "react-currency-formatter";
-import { useDispatch, useSelector } from "react-redux";
-import {
-  addToBasket,
-  removeFromBasket,
-  selectItems,
-} from "../slices/basketSlice";
+import { useDispatch } from "react-redux";
+import { addToBasket, removeFromBasket } from "../slices/basketSlice";
 
-function CheckoutItems({
+function CheckoutProduct({
   id,
   title,
-  rating,
   price,
   description,
   category,
   image,
-  hasprime,
+  hasPrime,
+  rating,
 }) {
   const dispatch = useDispatch();
-  const items = useSelector(selectItems);
-  const addingitems = () => {
+  const addItemToBasket = () => {
     const product = {
       id,
       title,
-      rating,
       price,
       description,
       category,
       image,
-      hasprime,
+      hasPrime,
+      description,
     };
+    //Push item into redux
     dispatch(addToBasket(product));
   };
-
-  const removingitems = () => {
+  const removeItemFromBasket = () => {
+      //remove item from redux
     dispatch(removeFromBasket({ id }));
   };
-
   return (
-    <div className="grid grid-cols-5 ">
-      <Image src={image} height={200} width={200} objectfit="contain" />
+    <div className="grid grid-cols-5">
+      <Image src={image} height={200} width={200} objectFit="contain" />
+
+      {/* Middle */}
       <div className="col-span-3 mx-5">
-        <p>{title}</p>
-        <div className="flex">
+        <p> {title} </p>
+        <div className="flex"></div>
+        <div>
           {Array(rating)
             .fill()
             .map((_, i) => (
               <StarIcon key={i} className="h-5 text-yellow-500" />
             ))}
         </div>
-
-        <div className="text-xs my-2 line-clamp-3">{description}</div>
-
-        <Currency quantity={price * 100} currency="INR" />
-
-        {hasprime && (
+        <p className="text-xs" my-2 line-clamp-3>
+          {" "}
+          {description}{" "}
+        </p>
+        <Currency quantity={price} Currency="GBP" />
+        {hasPrime && (
           <div className="flex items-center space-x-2">
-            <img
-              loading="lazy"
-              className="w-12"
-              src="https://links.papareact.com/fdw"
-            />
-            <p className="text-xs text-gray-500">FREE Next-day Delivery</p>
+            <img loading="lazy" src="https://links.papareact.com/fdw" alt="" />
+            <p className="text-xs text-gray-500">FREE Next-Day Delivery</p>
           </div>
         )}
       </div>
-      <div className="flex space-y-2 my-auto flex-col  justify-self-end">
-        <button onClick={addingitems} className="button text-xs ">
+      {/* Right add/remove buttons */}
+      <div className="flex flex-col space-y-2 my-auto justify-self-en">
+        <button className="button" onClick={addItemToBasket}>
           Add to Basket
         </button>
-        <button onClick={removingitems} className="button text-xs">
+        <button className="button" onClick={removeItemFromBasket}>
           Remove from Basket
         </button>
       </div>
     </div>
   );
 }
-
-export default CheckoutItems;
+export default CheckoutProduct;
