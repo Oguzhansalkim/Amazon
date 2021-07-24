05998c1d2100fd2db5fb67f5445c528c9838ef24
Oguzhan <75084633+Oguzhansalkim@users.noreply.github.com>
2021-07-24 21:26:55 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,38 @@
-import { getSession } from "next-auth/client";
 import Head from "next/head";
-import Banner from "../components/Banner";
+
 import Header from "../components/Header";
-import Productfeed from "../components/Productfeed";
+import Banner from "../components/Banner";
+import ProducFeed from "../components/ProducFeed";
 
 export default function Home({ products }) {
   return (
     <div className="bg-gray-100">
       <Head>
-        <title>Amazon 2.0</title>
+        <title>Shop Clever</title>
       </Head>
 
+      {/* Header  */}
       <Header />
 
-      <main className="max-w-screen-lg mx-auto">
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Banner */}
         <Banner />
 
-        <Productfeed products={products} />
+        {/* ProductFeed */}
+        <ProducFeed products={products} />
       </main>
     </div>
   );
 }
 
-export async function getServerSideProps(context) {
-  const session = await getSession(context);
+export async function getStaticProps(context) {
   const products = await fetch("https://fakestoreapi.com/products").then(
     (res) => res.json()
   );
 
   return {
     props: {
-      products: products,
-      session,
+      products,
     },
   };
 }
