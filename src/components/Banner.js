7961686fdb6d9b5ae33e020dabd914a4e60abb17
Oguzhan <75084633+Oguzhansalkim@users.noreply.github.com>
2021-07-24 13:07:50 +0300

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,32 +1,38 @@
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
+
+
 function Banner() {
-  return (
-    <div className="relative">
-      <div className="absolute h-32 w-full bottom-0 bg-gradient-to-t from-gray-100 to-transparent z-20" />
-      <Carousel
-        autoPlay
-        infiniteLoop
-        showStatus={false}
-        showIndicators={false}
-        showThumbs={false}
-        interval={5000}
-      >
-        <div>
-          <img loading="lazy" src="https://links.papareact.com/gi1" />
-        </div>
+    return (
+        <div className='relative'>
+            <div className="absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20" />
+            <Carousel
+                autoPlay
+                infiniteLoop
+                showStatus={false}
+                showIndicators={false}
+                showThumbs={false}
+                interval={5000}
+            >
+                <div>
+                    <img loading="lazy
+                    " src="https://links.papareact.com/gi1" alt="" />
 
-        <div>
-          <img loading="lazy" src="https://links.papareact.com/6ff" />
-        </div>
+                </div>
 
-        <div>
-          <img loading="lazy" src="https://links.papareact.com/7ma" />
+                <div>
+                    <img loading="lazy
+                    " src="https://links.papareact.com/6ff" alt="" />
+                </div>
+
+                <div>
+                    <img loading="lazy
+                    " src="https://links.papareact.com/7ma" alt="" />
+                </div>
+            </Carousel>
         </div>
-      </Carousel>
-    </div>
-  );
+    )
 }
 
-export default Banner;
+export default Banner
