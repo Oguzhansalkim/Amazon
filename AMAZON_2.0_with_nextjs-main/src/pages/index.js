368e8e25b078d729ec10fe888643c54402131939
developerogi <75084633+developerogi@users.noreply.github.com>
2021-07-24 09:32:38 +0300

    first commit

---
+++ AMAZON_2.0_with_nextjs-main/src/pages/index.js
@@ -0,0 +1,37 @@
+import { getSession } from "next-auth/client";
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import Productfeed from "../components/Productfeed";
+
+export default function Home({ products }) {
+  return (
+    <div className="bg-gray-100">
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+
+      <Header />
+
+      <main className="max-w-screen-lg mx-auto">
+        <Banner />
+
+        <Productfeed products={products} />
+      </main>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      products: products,
+      session,
+    },
+  };
+}
